4392accc2c50e8d776ba52af072c24976c843298
Brendan A R Sechter <sgeos@hotmail.com>
2021-04-19 20:03:25 +0900

    Added program state and split funtions out of main loop.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use palette::{Hsv, LinSrgb};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::render::Canvas;
 
 use std::error::Error;
 use std::ffi::CStr;
@@ -13,6 +14,32 @@ use std::thread::sleep;
 use std::time::Duration;
 
 const SLEEP_SECOND: u32 = 1_000_000_000;
+
+struct State {
+  done: bool,
+  fps: u32,
+  frame: u64,
+  background_color_base: ColorBase,
+  hsv_delta: Hsv,
+  hsv_offset: Hsv,
+  background_color: Hsv,
+}
+
+impl State {
+  pub fn new() -> Self {
+    let fps = 60;
+    let background_color_base = ColorBase::Red;
+    Self {
+      done: false,
+      fps,
+      frame: 0,
+      background_color_base,
+      hsv_delta: Hsv::new::<f32>(60.0 / fps as f32, 0.0, 0.0),
+      hsv_offset: Hsv::new::<f32>(0.0, 0.0, 0.0),
+      background_color: background_color_base.to_hsv(),
+    }
+  }
+}
 
 #[derive(Clone, Copy)]
 enum ColorBase {
@@ -52,93 +79,92 @@ pub extern fn run(argc: c_int, argv: *const *const c_char) {
 
 pub fn rlib_run(args: Vec<&str>) -> Result<(), Box<dyn Error>> {
   // Arguments
-  println!("Arguments:");
   for message in args {
-    println!("  {}", message);
+    println!("Program Argument: {}", message);
   }
   println!("");
 
   // SDL2 Setup
   let sdl_context = sdl2::init()?;
-  let video_subsystem = sdl_context.video()?;
-  let window_title = "SDL2 Template";
-  let window_width = 800;
-  let window_height = 600;
-  let window = video_subsystem
-    .window(window_title, window_width, window_height)
+  let sdl_video_subsystem = sdl_context.video()?;
+  let sdl_window_title = "SDL2 Template";
+  let sdl_window_width = 800;
+  let sdl_window_height = 600;
+  let sdl_window = sdl_video_subsystem
+    .window(sdl_window_title, sdl_window_width, sdl_window_height)
     .position_centered()
     .resizable()
     .build()?;
-  let mut canvas = window.into_canvas().build()?;
-
-  // Program State
-  let mut done = false;
-  let fps = 60;
-  let mut frame: u64 = 0;
-  let mut background_color_base = ColorBase::Red;
-  // hue degrees per second
-  let hsv_delta = Hsv::new::<f32>(60.0 / fps as f32, 0.0, 0.0);
-  let mut hsv_offset = Hsv::new::<f32>(0.0, 0.0, 0.0);
+  let mut sdl_canvas = sdl_window.into_canvas().build()?;
+  let mut sdl_event_pump = sdl_context.event_pump()?;
 
   // Main Loop
-  println!("SDL2 events:");
-  let mut event_pump = sdl_context.event_pump()?;
-  while !done {
-    // Process Events
-    for event in event_pump.poll_iter() {
-      let mut print = true;
-      match event {
-        Event::Quit { .. }
-        | Event::KeyDown {
-          keycode: Some(Keycode::Escape),
-          ..
-        } => done = true,
-        // skip mouse motion intentionally because of the verbose it might cause.
-        Event::MouseMotion { .. } => print = false,
-        _ => {},
-      }
-      if print {
-        println!("{:?}", event);
-      }
-    }
+  let mut state = State::new();
+  while !state.done {
+    input(&mut state, &mut sdl_event_pump);
+    update(&mut state);
+    output(&mut state, &mut sdl_canvas);
+  }
+  Ok(())
+}
 
-    // Update
-    let flash_interval = 7; // seconds between flashes
-    let flash_duration = 1; // flash frames
-    let flash = frame % u64::from(flash_interval * fps) < flash_duration;
-    if 0 == frame % u64::from(flash_interval * fps) {
-      background_color_base = match background_color_base {
-        ColorBase::Red => ColorBase::Green,
-        ColorBase::Green => ColorBase::Blue,
-        ColorBase::Blue => ColorBase::Red,
-        ColorBase::White => ColorBase::Black,
-        ColorBase::Black => ColorBase::White,
-      };
+fn input(state: &mut State, sdl_event_pump: &mut sdl2::EventPump) {
+  for event in sdl_event_pump.poll_iter() {
+    let mut print = true;
+    match event {
+      Event::Quit { .. }
+      | Event::KeyDown {
+        keycode: Some(Keycode::Escape),
+        ..
+      } => state.done = true,
+      // skip mouse motion intentionally because of the verbose it might cause.
+      Event::MouseMotion { .. } => print = false,
+      _ => {},
     }
-    hsv_offset = hsv_offset + hsv_delta;
-    let background_color = match flash {
-      true => ColorBase::White.to_hsv(), // flash color
-      false => background_color_base.to_hsv() + hsv_offset,
+    if print {
+      println!("SDL2 Event: {:?}", event);
+    }
+  }
+}
+
+fn update(state: &mut State) {
+  // Background Color Update
+  let flash_interval = 7; // seconds between flashes
+  let flash_duration = 1; // flash frames
+  let flash = state.frame % u64::from(flash_interval * state.fps) < flash_duration;
+  if 0 == state.frame % u64::from(flash_interval * state.fps) {
+    state.background_color_base = match state.background_color_base {
+      ColorBase::Red => ColorBase::Green,
+      ColorBase::Green => ColorBase::Blue,
+      ColorBase::Blue => ColorBase::Red,
+      ColorBase::White => ColorBase::Black,
+      ColorBase::Black => ColorBase::White,
     };
+  }
+  state.hsv_offset = state.hsv_offset + state.hsv_delta;
+  state.background_color = match flash {
+    true => ColorBase::White.to_hsv(), // flash color
+    false => state.background_color_base.to_hsv() + state.hsv_offset,
+  };
 
-    // Draw
-    canvas.set_draw_color(hsv_to_color(background_color));
-    canvas.clear();
-    canvas.present();
-    sleep(Duration::new(0, SLEEP_SECOND / fps));
-
-    // Diagnostic Output
-    let print_seconds = 60;
-    if 0 == frame % u64::from(print_seconds * fps) {
-      println!("Frame {} / HSV {:?}", frame, background_color);
-    }
-
-    // Frame Update
-    frame = frame.wrapping_add(1);
+  // Diagnostic Output
+  let print_seconds = 60;
+  if 0 == state.frame % u64::from(print_seconds * state.fps) {
+    println!("State Update: Frame {} => HSV {:?}",
+      state.frame,
+      state.background_color
+    );
   }
 
-  // Done
-  Ok(())
+  // Frame Update
+  state.frame = state.frame.wrapping_add(1);
+}
+
+fn output(state: &mut State, canvas: &mut Canvas<sdl2::video::Window>) {
+  canvas.set_draw_color(hsv_to_color(state.background_color));
+  canvas.clear();
+  canvas.present();
+  sleep(Duration::new(0, SLEEP_SECOND / state.fps));
 }
 
 // Convert from palette HSV to RGB color.
